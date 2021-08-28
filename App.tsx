ce2737bda194fdd8e2ebea53cf43520ccde719e8
joinerhs <herrerajoiner@gmail.com>
2021-08-28 09:54:51 -0500

    dojoReactNative1

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import { HelloWorldScreen } from './src/screens/helloWorld'
+import React from "react";
+import { Text, View } from "react-native";
+import { HelloWorldScreen } from "./src/screen/helloWorld";
+import { BasicCounterScreen } from "./src/screen/basic-counter";
+
 export default function App() {
   return (
-    <HelloWorldScreen></HelloWorldScreen>
+    //<HelloWorldScreen></HelloWorldScreen>
+    <BasicCounterScreen></BasicCounterScreen>
   );
 }
-
